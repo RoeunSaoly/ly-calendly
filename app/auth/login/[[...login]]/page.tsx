0372d37228b0c72c2cbd499eb346767fa6e63456
RoeunSaoly <saolyroeun@gmail.com>
2025-07-01 16:50:20 +0700

    committed

---
+++ app/auth/login/[[...login]]/page.tsx
@@ -0,0 +1,18 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+        <Image
+          src="/assets/logo.svg"
+          alt="Logo"
+          width={100}
+          height={100}
+        />
+        <div className="mt3">
+            <SignIn/>
+        </div>
+    </main>
+  );
+}
